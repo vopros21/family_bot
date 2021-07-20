1778b40fba76987298fdbb93521586e0f9bb0e93
Mike Kostenko <vopros.m@gmail.com>
2021-07-20 19:50:32 +0100

    Small updates portfolio_engine to work with new db_engine

--- portfolio_engine.py
+++ portfolio_engine.py
@@ -1,19 +1,9 @@
-import yfinance as yf
-from datetime import datetime
-
-
 # Get array of close prices for stock
 import db_engine as de
 
 
-def get_close_prices(stock, start_date='2020-01-01'):
-    today = datetime.today().strftime('%Y-%m-%d')
-    data = yf.download(stock, start_date, today)
-    return list(data.to_dict().get('Close').values())
-
-
 def get_close_price(stock):
-    return get_close_prices(stock)[-1]
+    return de.get_last_close_price(stock)
 
 
 # TODO: improve code not to use read_portfolio twice: here and in average price
@@ -46,15 +36,10 @@ def read_portfolio():
     portfolio_dict = {}
     portfolio = de.db_read_portfolio()
     for line in portfolio:
-        _, _, current_stock, date, price, number = line
+        current_stock, date, price, number = line
         if current_stock == 'TEST':
             continue
         stock_pn = portfolio_dict.get(current_stock, (0, 0))
         stock_pn = stock_pn[0] + price, stock_pn[1] + int(number)
         portfolio_dict[current_stock] = stock_pn
     return portfolio_dict
-
-
-if __name__ == '__main__':
-    print(average_price('aapl'))
-    print(get_plvalue('aapl'))
