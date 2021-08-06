09d5ad55b0905162f983c64a452a574002f2ab52
Mike Kostenko <vopros.m@gmail.com>
2021-08-06 21:25:05 +0100

    Starting plot engine

--- plot_engine.py
+++ plot_engine.py
@@ -1,6 +1,30 @@
 import re
 import matplotlib.pyplot as plt
 
+
+# form link to generated image
+def link_to_image(name: str):
+    return f'data/ple_images/{name}'
+
+
+# return path to image
+def print_profit_loss(tickers: list, time_period='1y'):
+    image_title = ''
+
+    return link_to_image(name=image_title)
+
+
+# TODO: create set of dates, ask PL for each date and form the result dictionary
+def get_ticker_data(ticker: str, time_period: str):
+
+    return {}
+
+
+# TODO: assemble information for several different tickers
+def get_all_data(time_period):
+    return {}
+
+
 if __name__ == '__main__':
     data = {'apple': 10, 'orange': 15, 'lemon': 5, 'lime': 20}
     names = list(data.keys())
@@ -11,6 +35,6 @@ if __name__ == '__main__':
     # axs[1].scatter(names, values)
     # axs[2].plot(names, values)
     fig.suptitle('Categorical Plotting')
-    plt.show()
-    # plt.savefig('graph.png')
+    # plt.show()
+    plt.savefig('data/ple_images/graph.png')
 
